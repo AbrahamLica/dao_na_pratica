abe42bf866f9095c96e06796ea325667df1ff059
Abraham Melquisedeque Pereira Licá <abrahamlica451@gmail.com>
2023-02-06 22:57:31 -0300

    Initial commit

---
+++ models/Usuario.php
@@ -0,0 +1,55 @@
+<?php
+class Usuario {
+    private $id;
+    private $nome;
+    private $email;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+
+    public function setNome($nome)
+    {
+        $this->nome = ucwords(trim($nome));
+
+        return $this;
+    }
+
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+
+    public function setEmail($email)
+    {
+        $this->email = strtolower(trim($email));
+
+        return $this;
+    }
+}
+
+interface UsuarioDAO {   
+    public function add(Usuario $u);
+    public function findAll();
+    public function findByEmail($email);
+    public function findById($id);
+    public function update(Usuario $u);
+    public function delete($id);
+}
